737eb6ff7921fddb4073c04592458ca9394b14e8
Saowaluck Suksawat <poppy@odds.team>
2021-05-29 20:53:47 +0700

    Add declear variable and zero value

--- main.go
+++ main.go
@@ -4,4 +4,34 @@ import "fmt"
 
 func main() {
 	fmt.Println("yohhh")
+	dataTypeAndFVariable()
+}
+
+func dataTypeAndFVariable() {
+	// static type
+	var s string
+	var i int
+	var f float64
+	var b bool
+
+	// zero
+	fmt.Println(s, i, f, b)
+
+	// assign
+	s = "hi"
+	i = 1
+	f = 1.0
+	b = false
+	fmt.Println(s, i, f, b)
+
+	// short declear variable
+	s1 := "hi"
+	i1 := 1
+	f1 := 1.0
+	b1 := false
+	fmt.Println(s1, i1, f1, b1)
+
+	// constance
+	const pi float64 = 3.17
+	fmt.Println(pi)
 }
